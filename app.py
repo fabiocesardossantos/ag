f34cfd224b15a13cbea1b528ae4619f496aa6186
fabiocesardossantos <169106895+fabiocesardossantos@users.noreply.github.com>
2024-05-13 13:07:19 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,20 @@
+import streamlit as st
+import pandas as pd
+from bcb import sgs 
+from datetime import datetime, timedelta
+
+
+
+
+indicadores =['x','y']
+
+
+
+
+
+#titulo
+st.write("""
+# My first app
+Hello *world!*
+""")
+
